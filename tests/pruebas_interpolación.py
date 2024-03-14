b397b75623b1f4ed3ebce75e079e95830f77f4f7
dome0luis0valentin <valentindome777@gmail.com>
2024-03-14 18:45:06 -0300

    P

--- tests/pruebas_interpolación.py
+++ tests/pruebas_interpolación.py
@@ -4,15 +4,30 @@ import matplotlib.pyplot as plt
 def probar(curvas):
     # x: 0.19 - 0.22
     # y: 43.0 - 84.0
+
+    # Resultados esperados:
+    # 11439.17427135962
+    # 11526.617961425418
+    # 11621.862838432991
+    # 11621.862838432991
+    # 11720.89565222022
+    
+    #Casos fijos:
+    xs = [0.20291153295793762, 0.20291153295793762, 0.20291153295793762, 0.20291153295793762, 0.20291153295793762]
+    ys = [4.77, 5.77, 6.77, 7.77, 8.77]
+    
     magnitudes = []
     ejex = []
     ejey = []
+    
+    for x,y in zip(xs,ys):
 
-    for i in range(4300, 8400, 100):
+    # for i in range(4300, 8400, 100):
     # if (True):
-        x = float(randint(1900, 2200)/10000)
-        y = float(randint(4300, 8400)/100)
-        y = i / 100
+        # x = float(randint(1900, 2200)/10000)
+        # y = float(randint(4300, 8400)/100)
+        # y = i / 100
+
         print(f"Prueba con {x , y}")
         ejex.append(x)
         ejey.append(y)
@@ -21,4 +36,6 @@ def probar(curvas):
         magnitudes.append(b)
 
     plt.scatter(ejex, ejey, color="red", marker=".")
-    print(f"Esta son la lista de magnitudes: ", b)
+    print(f"Esta son la lista de magnitudes: ", magnitudes)
+    for i in magnitudes:
+        print(i)
